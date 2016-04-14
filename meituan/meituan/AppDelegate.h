7a055e8d9a96ed39b3f21f3e783b08ef40727646
Like <Like@yebaodemacbook-pro.local>
2016-04-14 21:53:47 +0800

    开始项目

---
+++ meituan/meituan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  meituan
+//
+//  Created by Like on 16/4/14.
+//  Copyright © 2016年 Like. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ meituan/meituan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  meituan
+//
+//  Created by Like on 16/4/14.
+//  Copyright © 2016年 Like. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
