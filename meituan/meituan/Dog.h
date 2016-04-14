104771491a8ca628ecb7350b853778dac50abfeb
Like <Like@yebaodemacbook-pro.local>
2016-04-14 21:55:34 +0800

    添加dong类

---
+++ meituan/meituan/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  meituan
+//
+//  Created by Like on 16/4/14.
+//  Copyright © 2016年 Like. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ meituan/meituan/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  meituan
+//
+//  Created by Like on 16/4/14.
+//  Copyright © 2016年 Like. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
